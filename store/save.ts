0b75e7259a35688f5688ec6b078f473e56d73a32
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2021-09-25 19:35:10 +0100

    Added boilerplate for save store

---
+++ store/save.ts
@@ -0,0 +1,19 @@
+import {  } from 'vuex'
+
+const store = {
+  saveFile: 
+}
+
+const actions = {}
+
+const mutations = {}
+
+const getters = {}
+
+export default {
+  namespaced: true,
+  actions,
+  store,
+  mutations,
+  getters
+}
